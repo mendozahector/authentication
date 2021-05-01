00b32038b611b91a5e404f239013eb48d30b6323
Hector <hectormendoza@byui.edu>
2021-05-01 14:26:02 -0600

    first commit

---
+++ lab02.py
@@ -0,0 +1,16 @@
+# 1. Name:
+#    Hector Mendoza
+# 2. Assignment Name:
+#    Lab 02: Authentication
+# 3. Assignment Description:
+#    
+# 4. What was the hardest part? Be as specific as possible.
+#    
+# 5. How long did it take for you to complete the assignment?
+#    
+
+def main():
+    print("Hello world")
+
+if __name__ == "__main__":
+    main()
