97e6b175627d6add2c90422c67da34e67c653c49
Hector <hectormendoza@byui.edu>
2021-05-01 15:30:37 -0600

    user authentication completed

--- lab02.py
+++ lab02.py
@@ -9,8 +9,38 @@
 # 5. How long did it take for you to complete the assignment?
 #    
 
+import json
+
 def main():
-    print("Hello world")
+    data = get_data()
+
+    # We continue if database was loaded successfully
+    if data:
+        username = input("Username: ")
+        password = input("Password: ")
+
+        authenticate(data, username, password)
+
+def get_data():
+    # File is closed when the block inside the with statement
+    # is done executing
+    try:
+        with open('lab02.json', 'r') as json_file:
+            data = json.load(json_file)
+
+        return data
+    except:
+        print("Unable to open file lab02.json.")
+        return False
+
+def authenticate(data, username, password):
+    usernames = data['username']
+    passwords = data['password']
+
+    if username in usernames and password in passwords:
+        print("we have a match!")
+    else:
+        print("error login in.")
 
 if __name__ == "__main__":
     main()
